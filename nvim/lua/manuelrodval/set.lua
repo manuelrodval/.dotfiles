6a6d3e3bab317078ed345efb44887835e641f8ad
Manuel <manuelrodval@hotmail.com>
2023-08-08 08:24:22 -0500

    tabstop

--- nvim/lua/manuelrodval/set.lua
+++ nvim/lua/manuelrodval/set.lua
@@ -1,12 +1,13 @@
 -- Editor Options
-vim.opt.cursorline=true
-vim.opt.cursorcolumn=true
-vim.opt.number=true
-vim.opt.relativenumber=true
-vim.opt.statusline="%f"
-vim.opt.hlsearch=false
-vim.opt.completeopt='menuone,noselect'
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.statusline = "%f"
+vim.opt.hlsearch = false
+vim.opt.completeopt = "menuone,noselect"
 
 -- Tab autostop
-vim.opt.shiftwidth=4
-vim.opt.tabstop=4
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
